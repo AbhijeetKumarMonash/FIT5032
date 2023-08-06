a9052152042b0165ee7f538e986577e35c490395
Abhijeet Kumar <abhi0008@student.monash.edu>
2023-08-06 19:06:35 +1000

    Efolio 1 and 2 files

---
+++ repos/FIT5032_myCSS/FIT5032_myCSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_myCSS.MvcApplication" Language="C#" %>
---
+++ repos/FIT5032_myCSS/FIT5032_myCSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_myCSS.Startup))]
+namespace FIT5032_myCSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
